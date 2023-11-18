8cc673cadc5dfb9ed2a810f7463bf9f91578b38d
waynesdotdev <fox.wayne1@gmail.com>
2023-11-18 12:11:59 +0000

    css deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <div>
+      <div>
+        <Link href='/performance'>Performance</Link>
+        <Link href='/reliability'>Reliability</Link>
+        <Link href='/scale'>Scale</Link>
+      </div>
+      <h1>Home Page</h1>
+    </div>
+  )
 }
