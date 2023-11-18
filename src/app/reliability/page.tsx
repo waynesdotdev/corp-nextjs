44b365fb6062e7145b3225627e0e3e972a0d0e2d
waynesdotdev <fox.wayne1@gmail.com>
2023-11-18 12:04:21 +0000

    new pages added

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ReliabilityPage() {
+  return <div>Reliablity Page</div>
+}
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ScalePage() {
+  return <div>Scale Page</div>
+}
